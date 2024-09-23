9114d323ca41167011175a2a36246bd08588f05b
Sviat <swatoslaw112233@gmail.com>
2024-09-23 20:56:40 +0300

    first message

--- lab_0.idea/src/main/java/FirstClass.java
+++ lab_0.idea/src/main/java/FirstClass.java
@@ -1,6 +1,5 @@
-public class main {
+public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.printf("Hello World");
     }
 }

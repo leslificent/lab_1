68bc4923bc065bc6f4392731316d6c6f5ce8f909
leslificent <deineha.o.v@nmu.one>
2024-11-16 18:27:03 +0200

    Update FacultyCreator.java

--- lab_4/src/main/java/controller/FacultyCreator.java
+++ lab_4/src/main/java/controller/FacultyCreator.java
@@ -13,7 +13,7 @@ public class FacultyCreator implements Creator<Faculty, Department> {
     private final DepartmentCreator departmentCreator;
 
     public FacultyCreator() {
-        this.departmentCreator = new DepartmentCreator(); // Створення контролера для кафедр
+        this.departmentCreator = new DepartmentCreator();
     }
 
     @Override
@@ -21,8 +21,7 @@ public class FacultyCreator implements Creator<Faculty, Department> {
         return new Faculty(name, head, departments != null ? departments : List.of());
     }
 
-    // Можливість створення кафедри
     public Department createDepartment(String name, Human head, List<Group> groups) {
-        return departmentCreator.create(name, head, groups); // Делегуємо створення кафедри нижчому рівню
+        return departmentCreator.create(name, head, groups);
     }
 }

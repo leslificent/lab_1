d29cff60a34b44b26e25f958dd76f971c9f328c5
leslificent <deineha.o.v@nmu.one>
2024-11-16 18:26:05 +0200

    Update Creator.java

--- lab_4/src/main/java/controller/Creator.java
+++ lab_4/src/main/java/controller/Creator.java
@@ -6,6 +6,5 @@ import model.Human;
 import java.util.List;
 
 public interface Creator<T, E> {
-    // Метод для створення основних елементів, які підлягають управлінню
     T create(String name, Human head, List<E> subElements);
 }
